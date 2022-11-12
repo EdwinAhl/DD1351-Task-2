cd8c3f5485fdc4296a40651f0118191637b3cd4f
BaltzarL <106113033+BaltzarL@users.noreply.github.com>
2022-11-12 16:49:22 +0100

    add traversedlines

--- L2.pl
+++ L2.pl
@@ -1,4 +1,4 @@
-import() :- ['rules/*'].
+import() :- ['rules/*', 'utils/*'].
 
 % START ------------------------------------------------------
 
@@ -13,22 +13,35 @@ valid_proof(Prems, Goal, Proof).
 
 % READLINE -------------------------------------------------------------------------------------------------------------------
 
-readLine(_, _, _, []).
+% Arguments for readLine
+% 0: List of premises
+% 1: The Goal
+% 2: All Lines
+% 3: Traversed lines (including same and higher level assumptions)
+% 4: Current Line
 
 
 writeLine(A, B, C) :- write(A), write(" "), write(B), write(" "), write(C), write("\n").
 
+readLine(_, _, _, _, []).
+
 % When a sublist is reached then it should always be an assumption
-readLine(Prems, Goal, AllLines,
+readLine(Prems, Goal, AllLines, TraversedLines,
     [[[A, B, assumption]|Rest]|OuterRest]) :-
         writeLine(A, B, assumption),
-        readLine(Prems, Goal, AllLines, Rest), 
-        readLine(Prems, Goal, AllLines, OuterRest).
 
-readLine(Prems, Goal, AllLines, [[A,B,C]|Rest]) :- writeLine(A, B, C),
-    readLine(Prems, Goal, AllLines, Rest), valid_line(Prems, Goal, AllLines, A, B, C).
+        appendEl([A,B,assumption], TraversedLines, InnerTraversed),
+        readLine(Prems, Goal, InnerTraversed, AllLines, Rest),
 
-valid_proof(Prems, Goal, Line) :- readLine(Prems, Goal, Line, Line).
+        appendEl([[A,B,assumption]|Rest], TraversedLines, OuterTraversed),
+        readLine(Prems, Goal, AllLines, OuterTraversed, OuterRest).
+
+readLine(Prems, Goal, AllLines, TraversedLines, [[A,B,C]|Rest]) :- writeLine(A, B, C),
+    appendEl([A,B,C], TraversedLines, AllTraversed),
+    readLine(Prems, Goal, AllLines, AllTraversed, Rest),
+    valid_line(Prems, Goal, AllLines, TraversedLines, A, B, C).
+
+valid_proof(Prems, Goal, Line) :- readLine(Prems, Goal, Line, [], Line).
 
 % -----------------------------------------------------------------------------------------------------------------------------
 
@@ -40,6 +53,7 @@ valid_proof(Prems, Goal, Line) :- readLine(Prems, Goal, Line, Line).
 % 0: List of premises
 % 1: The Goal
 % 2: All Lines
+% 2: Traversed lines (not including the current one)
 % 3: (A) The Line Number (left)
 % 4: (B) The result (middle)
 % 5: (C) How the result was made (right)
@@ -47,19 +61,19 @@ valid_proof(Prems, Goal, Line) :- readLine(Prems, Goal, Line, Line).
 % Check validity of ...
 
 % premise 
-valid_line(Prems, _, _, _, Premise, premise) :- valid_premise(Prems, Premise).
+valid_line(Prems, _, _, _, _, Premise, premise) :- valid_premise(Prems, Premise).
 
 % impel 
-valid_line(_, _, AllLines, _, Result, impel(FirstLine, SecondLine)) :- impelSecondSlot(AllLines, AllLines, Result, FirstLine, SecondLine).
+valid_line(_, _, AllLines, _, _, Result, impel(FirstLine, SecondLine)) :- impelSecondSlot(AllLines, AllLines, Result, FirstLine, SecondLine).
 
 % negel 
-valid_line(_, _, AllLines, _, cont, negel(FirstLine, SecondLine)) :- negelFirstSlot(AllLines, AllLines, FirstLine, SecondLine).
+valid_line(_, _, AllLines, TraversedLines, _, cont, negel(FirstLine, SecondLine)) :- negelFirstSlot(TraversedLines, TraversedLines, FirstLine, SecondLine).
 
 % copy 
-valid_line(_, _, AllLines, _, Result, copy(LineNumber)) :- valid_copy(AllLines, Result, LineNumber).
+valid_line(_, _, AllLines, _, _, Result, copy(LineNumber)) :- valid_copy(AllLines, Result, LineNumber).
 
 % andint
-valid_line(_,_, AllLines, _, Result, andint(FirstLine, SecondLine)) :- valid_andint(AllLines, Result, FirstLine, SecondLine).
+valid_line(_,_, AllLines, _, _, Result, andint(FirstLine, SecondLine)) :- valid_andint(AllLines, Result, FirstLine, SecondLine).
 
 
 
